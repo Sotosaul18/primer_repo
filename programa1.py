7b1d8f4fc6db0b11fc2ea06cf525896259308c33
Sotosaul18 <a385853@uach.mx>
2025-10-01 08:19:08 -0700

    Create programa1.py

---
+++ programa1.py
@@ -0,0 +1,6 @@
+#Este es un script de python de prueba para el repositario
+def saludo(nombre):
+  return "Hola,"+nombre
+
+# se invoca a la función saludo para saludar a alguien llamado Carlos
+saludo("Carlos")
